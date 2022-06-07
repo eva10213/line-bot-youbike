906bd08f6e4095bddd3a745fb4a80847eea44116
Eva <s881123559978@gmail.com>
2022-06-07 23:39:28 +0800

    test2

--- ttt.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url= "https://api.kcg.gov.tw/api/service/Get/b4dd9c40-9027-4125-8666-06bef1756092"
-html = requests.get(url)
-html.encoding = 'utf-8'
-# print(html.text)
-dic = json.loads(html.text)
-print(dic.keys())
